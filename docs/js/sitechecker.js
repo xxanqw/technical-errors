84342e946088998ed70723d90d7284c69450eb0d
xxanqw <potik217@gmail.com>
2024-01-20 23:18:47 +0200

    ще не 21 число але мені пофік, ось докладність

---
+++ docs/js/sitechecker.js
@@ -0,0 +1,15 @@
+fetch('http://example.com/movies.json')
+  .then(response => {
+    if (!response.ok) {
+      throw new Error('Network response was not ok');
+    }
+    return response.json();
+  })
+  .then(data => {
+    console.log(data);
+    document.getElementById('isok').innerHTML = '✅'; // Виведіть іконку успіху
+  })
+  .catch(error => {
+    console.error('There has been a problem with your fetch operation:', error);
+    document.getElementById('isok').innerHTML = '❎'; // Виведіть іконку помилки
+  });
