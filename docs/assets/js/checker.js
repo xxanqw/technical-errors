c53e0051502fdc93c462ec435036f79c0d029866
xxanqw <potik217@gmail.com>
2024-01-24 16:38:14 +0200

    повний рефракторинг сайту, та відстеження статусу сайтів

---
+++ docs/assets/js/checker.js
@@ -0,0 +1,28 @@
+const site = "https://xxanqw.pp.ua";
+const server = "http://serveritself.xxanqw.pp.ua";
+
+fetch(site)
+  .then((response) => {
+    if (response.status === 200) {
+      //console.log("Сторінка доступна");
+      document.getElementById("site").innerHTML = "✅";
+    } else {
+      console.log("Сторінка недоступна");
+    }
+  })
+  .catch((error) => {
+    console.log("Помилка:", error);
+  });
+
+  fetch(server)
+  .then((response) => {
+    if (response.status === 200) {
+      //console.log("Сторінка доступна");
+      document.getElementById("server").innerHTML = "✅";
+    } else {
+      console.log("Сторінка недоступна");
+    }
+  })
+  .catch((error) => {
+    console.log("Помилка:", error);
+  });
