05676f61049df6b5ccb0def8f89f45a35b3cf8d7
xxanqw <potik217@gmail.com>
2024-01-24 20:53:44 +0200

    наразі все погано.

--- docs/assets/js/checker.js
+++ docs/assets/js/checker.js
@@ -1,5 +1,7 @@
 const site = "https://xxanqw.pp.ua";
 const server = "https://serveritself.xserv.pp.ua";
+const karpati = "https://karpatimoped.pp.ua";
+const hudoliy = "https://hudoliy.v.ua";
 
 fetch(site)
   .then((response) => {
@@ -11,7 +13,7 @@ fetch(site)
     }
   })
   .catch((error) => {
-    console.log("Помилка:", error);
+    console.log("Помилка:", error, site);
   });
 
   fetch(server)
@@ -24,5 +26,31 @@ fetch(site)
     }
   })
   .catch((error) => {
-    console.log("Помилка:", error);
+    console.log("Помилка:", error, server);
+  });
+
+  fetch(karpati)
+  .then((response) => {
+    if (response.status === 200) {
+      //console.log("Сторінка доступна");
+      document.getElementById("karpati").innerHTML = "✅";
+    } else {
+      console.log("Сторінка недоступна");
+    }
+  })
+  .catch((error) => {
+    console.log("Помилка:", error, karpati);
+  });
+
+  fetch(hudoliy)
+  .then((response) => {
+    if (response.status === 200) {
+      //console.log("Сторінка доступна");
+      document.getElementById("hudoliy").innerHTML = "✅";
+    } else {
+      console.log("Сторінка недоступна");
+    }
+  })
+  .catch((error) => {
+    console.log("Помилка:", error, hudoliy);
   });
