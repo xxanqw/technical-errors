fe33ad77056b9b143e415c0a3327331814009bb0
xxanqw <potik217@gmail.com>
2024-02-28 15:00:55 +0200

    Оновлення сайту (ver 2.01)

--- docs/assets/js/checker.js
+++ docs/assets/js/checker.js
@@ -8,83 +8,88 @@ fetch(site)
   .then((response) => {
     if (response.status === 200) {
       //console.log("Сторінка доступна");
-      document.getElementById("site").innerHTML = "✅";
-      document.getElementById("site-more").innerHTML = "✅" + response.status;
+      document.getElementById("site").innerHTML = "✅ Доступний";
+      document.getElementById("site-more").innerHTML = "✅ " + response.status;
     } else {
       console.log("Сторінка недоступна " + site);
-      document.getElementById("site").innerHTML = "❎";
+      document.getElementById("site").innerHTML = "❎ Недоступний";
+      document.getElementById("site-more").innerHTML = "❎ Сервер існує, але не дає відповіді";
     }
   })
   .catch((error) => {
     console.log("Помилка:", error, site);
-    document.getElementById("site-more").innerHTML = "❎" + error;
+    document.getElementById("site-more").innerHTML = "❎ " + error;
   });
-    document.getElementById("site").innerHTML = "❎";
+    document.getElementById("site").innerHTML = "❎ Недоступний";
 
   fetch(server)
   .then((response) => {
     if (response.status === 200) {
       //console.log("Сторінка доступна");
-      document.getElementById("server").innerHTML = "✅";
-      document.getElementById("server-more").innerHTML = "✅" + response.status;
+      document.getElementById("server").innerHTML = "✅ Доступний";
+      document.getElementById("server-more").innerHTML = "✅ " + response.status;
     } else {
       console.log("Сторінка недоступна " + server);
-      document.getElementById("server").innerHTML = "❎";
+      document.getElementById("server").innerHTML = "❎ Недоступний";
+      document.getElementById("server-more").innerHTML = "❎ Сервер існує, але не дає відповіді";
     }
   })
   .catch((error) => {
     console.log("Помилка:", error, server);
-    document.getElementById("server-more").innerHTML = "❎" + error;
-    document.getElementById("server").innerHTML = "❎";
+    document.getElementById("server-more").innerHTML = "❎ " + error;
+    document.getElementById("server").innerHTML = "❎ Недоступний";
   });
 
   fetch(karpati)
   .then((response) => {
     if (response.status === 200) {
       //console.log("Сторінка доступна");
-      document.getElementById("karpati").innerHTML = "✅";
-      document.getElementById("karpati-more").innerHTML = "✅" + response.status;
+      document.getElementById("karpati").innerHTML = "✅ Доступний";
+      document.getElementById("karpati-more").innerHTML = "✅ " + response.status;
     } else {
       console.log("Сторінка недоступна " + karpati);
-      document.getElementById("karpati").innerHTML = "❎";
+      document.getElementById("karpati").innerHTML = "❎ Недоступний";
+      document.getElementById("karpati-more").innerHTML = "❎ Сервер існує, але не дає відповіді";
     }
   })
   .catch((error) => {
     console.log("Помилка:", error, karpati);
-    document.getElementById("karpati-more").innerHTML = "❎" + error;
-    document.getElementById("karpati").innerHTML = "❎";
+    document.getElementById("karpati-more").innerHTML = "❎ " + error;
+    document.getElementById("karpati").innerHTML = "❎ Недоступний";
   });
 
   fetch(hudoliy)
   .then((response) => {
     if (response.status === 200) {
       //console.log("Сторінка доступна");
-      document.getElementById("hudoliy").innerHTML = "✅";
-      document.getElementById("hudoliy-more").innerHTML = "✅" + response.status;
+      document.getElementById("hudoliy").innerHTML = "✅ Доступний";
+      document.getElementById("hudoliy-more").innerHTML = "✅ " + response.status;
     } else {
       console.log("Сторінка недоступна " + hudoliy);
-      document.getElementById("hudoliy").innerHTML = "❎";
+      document.getElementById("hudoliy").innerHTML = "❎ Недоступний";
+      document.getElementById("hudoliy-more").innerHTML = "❎ Сервер існує, але не дає відповіді";
     }
   })
   .catch((error) => {
     console.log("Помилка:", error, hudoliy);
-    document.getElementById("hudoliy-more").innerHTML = "❎" + error;
-    document.getElementById("hudoliy").innerHTML = "❎";
+    document.getElementById("hudoliy-more").innerHTML = "❎ " + error;
+    document.getElementById("hudoliy").innerHTML = "❎ Недоступний";
   });
 
   fetch(serverweb)
   .then((response) => {
     if (response.status === 200) {
       //console.log("Сторінка доступна");
-      document.getElementById("serverweb").innerHTML = "✅";
-      document.getElementById("serverweb-more").innerHTML = "✅" + response.status;
+      document.getElementById("serverweb").innerHTML = "✅ Доступний";
+      document.getElementById("serverweb-more").innerHTML = "✅ " + response.status;
     } else {
       console.log("Сторінка недоступна " + serverweb);
-      document.getElementById("serverweb").innerHTML = "❎";
+      document.getElementById("serverweb").innerHTML = "❎ Недоступний";
+      document.getElementById("serverweb-more").innerHTML = "❎ Сервер існує, але не дає відповіді";
     }
   })
   .catch((error) => {
     console.log("Помилка:", error, serverweb);
-    document.getElementById("serverweb-more").innerHTML = "❎" + error;
-    document.getElementById("serverweb").innerHTML = "❎";
+    document.getElementById("serverweb-more").innerHTML = "❎ " + error;
+    document.getElementById("serverweb").innerHTML = "❎ Недоступний";
   });
